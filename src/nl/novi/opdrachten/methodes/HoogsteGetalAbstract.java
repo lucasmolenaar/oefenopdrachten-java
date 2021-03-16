23c2d097d09b945d2133110b40fd119a660a3606
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-03-16 19:16:37 +0100

    AbstractHoogsteGetal oefening gemaakt, zit nog een fout in

--- src/nl/novi/opdrachten/methodes/HoogsteGetalAbstract.java
+++ src/nl/novi/opdrachten/methodes/HoogsteGetalAbstract.java
@@ -1,5 +1,8 @@
 package nl.novi.opdrachten.methodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Schrijf een methode  die een List<Integer> ontvangt.
  *
@@ -11,4 +14,41 @@ package nl.novi.opdrachten.methodes;
  *
  */
 public class HoogsteGetalAbstract {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(60);
+        numbers.add(35);
+        numbers.add(2);
+        numbers.add(45);
+        numbers.add(320);
+        numbers.add(5);
+
+        System.out.println("Array Before Sorting");
+        for(int i=0; i < numbers.size(); i++){
+            System.out.print(numbers.get(i) + " ");
+        }
+        System.out.println();
+
+        sortNumbers(numbers);//sorting array elements using bubble sort
+
+        System.out.println("Array After Sorting");
+        for(int i=0; i < numbers.size(); i++){
+            System.out.print(numbers.get(i) + " ");
+        }
+    }
+
+    public static void sortNumbers(List<Integer> numbers) {
+        int temp = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = 1; j < (numbers.size() - i); j++) {
+                if (numbers.get(j - 1) > numbers.get(j)) {
+                    // Swap element
+                    temp = numbers.get(j-1);                    //
+                    numbers.get(j - 1) = numbers.get(j);       // HIER ZIT ERGENS EEN ERROR
+                    numbers.get(j) = temp;                     //
+                }
+            }
+        }
+    }
 }
